5a783011b1f5892b81c66bacd0e77be9f104a156
Oriana Mendez <egleemendez@gmail.com>
2024-07-29 15:09:07 +0200

    post /payments endpoint
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/status", (req, res) => {
   res.sendStatus(200);
+});
+
+app.post("/payments", (req, res) => {
+  res.json(req.body);
 });
 
 app.listen(port, () => {
