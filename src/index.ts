82c996dab63d20206c736a7b6d185da2baf5cf32
Oriana Mendez <egleemendez@gmail.com>
2024-07-29 11:17:19 +0200

    status endpoint
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/status", (req, res) => {
+  res.sendStatus(200);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
