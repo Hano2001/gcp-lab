c2c1b39b070a982783c40108c2085380dc289d8a
Oriana Mendez <egleemendez@gmail.com>
2024-07-30 10:37:52 +0200

    add bunyan
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
-import express from "express";
+import express, { NextFunction } from "express";
+import bunyan from "bunyan";
+import { v4 } from "uuid";
+
+const log = bunyan.createLogger({
+  name: "gcp-lab",
+  serializers: bunyan.stdSerializers,
+});
+
 const app = express();
 const port = 3000;
+
+app.use((req: any, res, next: NextFunction) => {
+  req.log = log.child({ req_id: v4() }, true);
+  req.log.info({ req: req });
+  res.on("finish", () => req.log.info({ res }));
+  next();
+});
 
 app.use(express.json());
 
@@ -18,4 +33,9 @@ app.post("/payments", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.use((err: any, req: any, res: any, next: NextFunction) => {
+  req.log.error({ err });
+  res.status(500).json("Internal server error");
 });
